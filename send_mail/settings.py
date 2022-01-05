1ba7633302d7ef43bcf3744321679a72b90f6a6f
Your Name <you@example.com>
2022-01-05 19:47:35 +0600

    q

--- send_mail/settings.py
+++ send_mail/settings.py
@@ -16,6 +16,7 @@ import django_heroku
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+django_heroku.settings(locals())
 
 
 # Quick-start development settings - unsuitable for production
@@ -165,4 +166,3 @@ CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com/','https://*.127.0.0.1']
 
 
 
-django_heroku.settings(locals())
