8870bac81e2cead743177b54ef492a039aff61bf
Your Name <you@example.com>
2022-01-05 17:00:54 +0600

    q

--- send_mail/settings.py
+++ send_mail/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%(-_vjuc2ewlms@eb@@ewibr&#lp#y19(7gi&q8dll4!4g3bsv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['send123123.herokuapp.com/','127.0.0.1:8000']
+ALLOWED_HOSTS = ['send1234.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
